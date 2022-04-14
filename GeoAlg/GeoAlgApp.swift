6a7b6b0e8b3a18dbabff287c6904458f9dd37769
miche <miche@mac.com>
2022-04-15 00:18:53 +0900

    Initial Commit

---
+++ GeoAlg/GeoAlgApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoAlgApp.swift
+//  GeoAlg
+//
+//  Created by Mitsuhiro Kondo on 2022/04/15.
+//
+
+import SwiftUI
+
+@main
+struct GeoAlgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
